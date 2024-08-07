b44f305fbe1a1f474514637f7e5c51de73b3da3b
Kailash <kailash.doodleblue@gmail.com>
2024-08-07 11:26:40 +0530

    changes

--- index.js
+++ index.js
@@ -6,23 +6,17 @@ const app = express();
 const server = http.createServer(app);
 
 const io = socketio(server)
-// route
 app.get('/', (req,res) => {
-    res.sendFile(__dirname+ '/indexx.html');
+    res.sendFile(__dirname+ '/index.html');
 })
 
-// handle socket connection
 io.on('connection' , (socket) => {
     console.log('A new user connected');
-    // handle incomming message
     socket.on('message', (message) => {
         console.log('Message received: ', message);
-        // Brodcast this message to all connected users
         io.emit('message', message)
         
     });
-
-    // Handle disconnection
     socket.on('disconnect', () => {
         console.log('A user disconnected')
     })
